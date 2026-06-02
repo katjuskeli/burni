0e82c3d544b4f04408a52c233917e5e90e76dd08
katjuskeli <katjuskeli@outlook.com>
2026-06-02 20:43:27 +0300

    Update aikataulu.js

--- js/aikataulu.js
+++ js/aikataulu.js
@@ -17,14 +17,14 @@ const schedule = [
     label: "Kannattaa liittyä mukaan kotoa käsin"
   },
   {
-    time: "TIISTAINA KLO 12:00",
+    time: "TIISTAINA KLO 12:00 - JATKUU KESKIVIIKKONA AAMUJOOGAN JÄLKEEN",
     title: "KÄSITYÖNURKKA",
     label: "Riittääkö sorminäppäryys ja hermot, kun pitää koota miniatyyri kasvihuone?"
   },
   {
     time: "KESKIVIIKKONA",
     title: "YHDEN TÄHDEN ILLALLINEN",
-    label: "Huippukokki on keksinyt Burnille kokattavaksi 3-ruokalajin menun."
+    label: "Huippukokki on keksinyt Burnille kokattavaksi 3-ruokalajin menun. Menu: !illallinen chättiin."
   },
   {
     time: "TORSTAINA",
