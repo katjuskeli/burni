55a892bfca82ae7eb815a973fb0aff0d3de3fc3a
katjuskeli <katjuskeli@outlook.com>
2026-06-08 02:03:32 +0300

    Update aikataulu.js

--- js/aikataulu.js
+++ js/aikataulu.js
@@ -15,15 +15,5 @@ const schedule = [
     time: "JOKA AAMU KLO 9:00",
     title: "AAMUJOOGA",
     label: "Kannattaa liittyä mukaan kotoa käsin"
-  },
-  {
-    time: "SUNNUNTAINA 12:30 ISH",
-    title: "NAPAKYMPPI",
-    label: "Kaikkien rakastama kultakutri palaa etsimään rakkautta. Mutta löytääkö hän sen?"
-  },
-  {
-    time: "SUNNUNTAINA 17:30->",
-    title: "SPORTTIVARTTI JOS TOINENKIN",
-    label: "Kuinka Burni selviää 77 kilometrin pyöräilystä kahden huippu-urheilijan kanssa?"
   }
 ];
